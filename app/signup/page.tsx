436ff35cc188fb5eb496fe56bc9d10c4bacee54d
Siraajadams <siraaj.adams2003@gmail.com>
2026-06-05 12:37:03 +0200

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -11,11 +11,7 @@ export default function SignupPage() {
 
   async function signup() {
     setMessage("");
-    const { error } = await supabase.auth.signUp({
-  email,
-  password,
-  emailRedirectTo: `${window.location.origin}/login`
-});
+    const { error } = await supabase.auth.signUp({ email, password, options: { emailRedirectTo: `${window.location.origin}/login`, }, });
     if (error) setMessage(error.message);
     else setMessage("Check your email to confirm your account, then log in.");
   }
